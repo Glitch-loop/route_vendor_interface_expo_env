22888e6b2a178ee46cd40f37486d2b5a8632df38
Glitch-loop <renetdejesus@gmail.com>
2026-02-16 15:16:49 -0600

    Set an standard height for the tables

--- utils/inventoryOperationTableStyles.ts
+++ utils/inventoryOperationTableStyles.ts
@@ -1,11 +1,11 @@
-export const headerTitleTableStyle:string = 'flex flex-row justify-center items-center';
-export const viewTagHeaderTableStyle:string = 'flex flex-row justify-center items-center';
+export const headerTitleTableStyle:string = 'h-12 flex flex-row justify-center items-center';
+export const viewTagHeaderTableStyle:string = 'h-12 flex flex-row justify-center items-center';
 export const textHeaderTableStyle:string = 'text-center text-black text-base';
 
 export const rowTableStyle:string = '';
-export const cellTableStyle:string = 'flex flex-row justify-center items-center';
-export const cellTableStyleWithAmountOfProduct:string = 'flex flex-row justify-center items-center bg-amber-100';
-export const viewTagRowTableStyle:string = 'flex flex-row items-center justify-center';
+export const cellTableStyle:string = 'h-12 flex flex-row justify-center items-center';
+export const cellTableStyleWithAmountOfProduct:string = 'h-12 flex flex-row justify-center items-center bg-amber-100';
+export const viewTagRowTableStyle:string = 'h-12 flex flex-row items-center justify-center';
 export const textRowTableStyle:string = 'text-center text-black flex flex-row justify-center';
 
 
