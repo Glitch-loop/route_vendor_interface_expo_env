a2552742b4f3508f73c255ed4e22af615b883578
Glitch-loop <renetdejesus@gmail.com>
2026-03-08 18:11:04 -0600

    Defining initial coordinates for search client and create client layout

--- utils/constants/initialCoordinates.ts
+++ utils/constants/initialCoordinates.ts
@@ -1,10 +1,8 @@
 import { LatLng } from 'react-native-maps';
 
 const INITIAL_COORDINATES:LatLng = {
-    // latitude: 20.643370742890433, 
-    // longitude: -103.39182090759277
-    latitude: 20.648999944123357, 
-    longitude: -105.2319590088954
+    latitude: 20.643370742890433, 
+    longitude: -103.39182090759277
 }
 
 export default INITIAL_COORDINATES;
