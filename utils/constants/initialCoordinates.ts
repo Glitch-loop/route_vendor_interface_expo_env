e05d0414b7001d96e3ac72cf1576de62e6d3a96a
Glitch-loop <renetdejesus@gmail.com>
2026-06-22 18:09:47 -0600

    Changing initial coordinates

--- utils/constants/initialCoordinates.ts
+++ utils/constants/initialCoordinates.ts
@@ -1,8 +1,8 @@
 import { LatLng } from 'react-native-maps';
 
 const INITIAL_COORDINATES:LatLng = {
-    latitude: 20.643370742890433, 
-    longitude: -103.39182090759277
+    latitude: 20.63918047291711, 
+    longitude: -105.22175216879452
 }
 
 export default INITIAL_COORDINATES;
