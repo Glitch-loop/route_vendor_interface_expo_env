3a65c9732dbef1d02f1f495fbb692ce9923f88b5
Glitch-loop <renetdejesus@gmail.com>
2026-09-13 10:44:57 -0600

    Adding label for letting know the user the method of operation that he will select

--- app/selectionInventoryOperationMethodLayout.tsx
+++ app/selectionInventoryOperationMethodLayout.tsx
@@ -1,8 +1,8 @@
 // Libraries
 import tw from 'twrnc';
 import React from 'react';
-import { View } from 'react-native';
-import { Href, Router, useLocalSearchParams, useRouter } from 'expo-router';
+import { View, Text } from 'react-native';
+import { Router, useLocalSearchParams, useRouter } from 'expo-router';
 
 // UI
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -11,6 +11,7 @@ import RouteHeader from '@/components/shared-components/RouteHeader';
 // Utils
 import { DAY_OPERATIONS } from '@/src/core/enums/DayOperations';
 import ProjectButton from '@/components/shared-components/ProjectButton';
+import { getTitleDayOperation } from '@/utils/day-operation/utils';
 
 
 /*
@@ -67,13 +68,21 @@ const selectionInventoryOperationMethodLayout = () => {
   return (
     <SafeAreaView>
       <View style={tw`w-full h-full flex flex-col items-center`}>
-        <View style={tw`mt-3 w-full`}>
+        <View style={tw`mt-3 flex basis-1/12 w-full`}>
           <RouteHeader
             onGoBack={handlerGoBack}/>
         </View>
-        <View style={tw`w-full h-full flex flex-row items-center justify-center`}>
+        <View style={tw`flex basis-2/12 flex-col items-center`}>
+          <Text style={tw`text-center text-black text-2xl`}>
+            { getTitleDayOperation(inventory_operation_type, null) }
+          </Text>
+          <Text style={tw`mt-3 text-center text-black text-xl`}>
+            Escoge el metodo para registro de inventario
+          </Text>
+        </View>
+        <View style={tw`w-full flex basis-9/12 flex-row items-start justify-center`}>
           <ProjectButton 
-            title={'Auto registro de inventario'}
+            title={'Registro manual de inventario.'}
             onPress={handlerGoToInventoryWithManualMethod}
             buttonVariant={'indigo'}
             textStyle='text-2xl text-center text-white'
